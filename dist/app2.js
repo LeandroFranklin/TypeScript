4992127b6c2b510f0e4ddaa743e62793fd1a15d6
Leandro Franklin <leandrofrankss5@gmail.com>
2023-12-08 16:22:21 -0300

    Classe Pessoa

--- dist/app2.js
+++ dist/app2.js
@@ -27,3 +27,21 @@ class Concessionaria {
         return this.listaDeCarros;
     }
 }
+class Pessoa {
+    constructor(nome, carroPreferido) {
+        this.nome = nome;
+        this.carroPreferido = carroPreferido;
+    }
+    dizerNome() {
+        return this.nome;
+    }
+    dizerCarroPreferido() {
+        return this.carroPreferido;
+    }
+    comprarCarro(carro) {
+        this.carro = carro;
+    }
+    dizerCarroQueTem() {
+        return this.carro;
+    }
+}
