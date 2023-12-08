f6a6c96c954cdd9857ef5810182d463eac524074
Leandro Franklin <leandrofrankss5@gmail.com>
2023-12-08 14:34:20 -0300

    Inferência de tipos

---
+++ dist/inferencia-tipo.js
@@ -0,0 +1,6 @@
+"use strict";
+let mensagem = "Seja Bem-Vindo!";
+let temporadaFriends = 10;
+let estudandoAngular = true;
+let listaDeFrutas = ["Uva", "Banana", "Abacaxi"];
+let notasDasProvas = [7, 5, 8, 9];
---
+++ inferencia-tipo.ts
@@ -0,0 +1,5 @@
+let mensagem:string = "Seja Bem-Vindo!";
+let temporadaFriends:number = 10;
+let estudandoAngular:boolean = true;
+let listaDeFrutas:string[] = ["Uva", "Banana", "Abacaxi"];
+let notasDasProvas:number[] = [7, 5, 8, 9];
